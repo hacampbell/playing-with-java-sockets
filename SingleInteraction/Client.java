2a00024d4c0500d55ad03e70dffde60feb0f8e86
Henry Campbell <hacampbell99@gmail.com>
2021-11-16 19:41:42 +1100

    Added header comment

--- SingleInteraction/Client.java
+++ SingleInteraction/Client.java
@@ -5,9 +5,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
+/** 
+ *  A simple single interaction client socket. This was made purely as a way
+ *  for me to get a basic introduction to sockets in java, as well as to act as
+ *  a jumping off point for future work.
+ * 
+ *  @author     Henry Campbell
+ */
+
 public class Client {
     private static final int PORT = 53044;
     private static final String ADDRESS = "127.0.0.1";
+    
     public static void main (String[] args) throws IOException {
         // create our socket to connect to the server with
         Socket connection = new Socket(ADDRESS, PORT);
