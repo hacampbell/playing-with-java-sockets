fa4d0e26027e3c4cfb43d6f7958d5a62a0b6ccfd
Henry Campbell <hacampbell99@gmail.com>
2021-11-16 19:38:55 +1100

    Added loop to allow server to keep running after the client closes

--- SingleInteraction/Server.java
+++ SingleInteraction/Server.java
@@ -5,30 +5,44 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/** 
+ *  A simple, single client, single interaction socket server. This was made 
+ *  purely as a way for me to get a basic introduction to sockets in java, as 
+ *  well as to act as a jumping off point for future work.
+ * 
+ *  @author     Henry Campbell
+ */
+
 public class Server {
     private static final int PORT = 53044; 
 
+    // We'll hide our resource leak warning for this little toybox project
+    @SuppressWarnings({"resource"})
     public static void main (String[] args) throws IOException {
         // Create the server socket we listen for connections on
         ServerSocket serverSocket = new ServerSocket(PORT);
         PrintLog("Server listening on port" + PORT);
 
-        // Accept a client connection when we get one
-        Socket client = serverSocket.accept();
-        PrintLog("Client Connected...");
+        while (true) {  // Loop so we can keep taking connections
+            
+            // Accept a client connection when we get one
+            Socket client = serverSocket.accept();
+            PrintLog("Client Connected...");
 
-        // Create a PritnWriter that allows us to send data to the client
-        PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+            // Create a PritnWriter that allows us to send data to the client
+            PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
 
-        // Write some simple data
-        writer.println("Hello World!");
-        PrintLog("Sending data (\"Hello World\")");
+            PrintLog("Sending data (\"Hello World\")");
+            writer.println("Hello World!");     // Write some simple data
+            client.close();
+        }
 
 
-        // Close the server and client sockets
-        PrintLog("Closing sockets");
-        client.close();
-        serverSocket.close();
+        /* Close the server socket, but we'll comment this out because it isn't
+           actually reachable due to the while loop */
+        
+        // PrintLog("Closing Server Socket");
+        // serverSocket.close();
     }
 
 
