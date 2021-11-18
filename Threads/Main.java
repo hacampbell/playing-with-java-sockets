3d4a9b0ccf22e48193827ff9c63122b40a09df56
Henry Campbell <hacampbell99@gmail.com>
2021-11-19 09:25:46 +1100

    Added a threaded input/output example

--- Threads/Main.java
+++ Threads/Main.java
@@ -5,18 +5,42 @@
  *  @author     Henry Campbell
  */
 
-package Threads; 
+package Threads;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
+        /* Example of having an input and output thread */
+        Scanner scanner = new Scanner(System.in);
+
         try {
-            for (int i = 0; i < 5; i++) {
-                Worker worker = new Worker();
-                worker.start();
+            String message = "";
+            Worker worker = new Worker();
+            
+            worker.start();
+
+            // There is a race condition on the console output, but for this
+            // simple example that's not really a concern
+            while (!message.equals("quit")) {
+                message = scanner.nextLine();
+                System.out.println(message);
             }
         } catch (Exception e) {
-            System.out.println("Something went wrong!");
-            System.out.println(e.toString());
+            System.out.print(e.getStackTrace());
+        } finally {
+            scanner.close();
         }
+
+        /* Example of having multiple worker threads */
+        // try {
+        //     for (int i = 0; i < 5; i++) {
+        //         Worker worker = new Worker();
+        //         worker.start();
+        //     }
+        // } catch (Exception e) {
+        //     System.out.println("Something went wrong!");
+        //     System.out.println(e.toString());
+        // }
     }
 }
